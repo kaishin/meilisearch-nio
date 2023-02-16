3f25dcdd5396869b36d78e801255b637572a3e13
Reda Lemeden <git@redalemeden.com>
2023-02-16 02:13:37 +0100

    Update search parameters

--- Sources/MeilisearchNIO/Models/SearchParameters.swift
+++ Sources/MeilisearchNIO/Models/SearchParameters.swift
@@ -1,63 +1,116 @@
 import Foundation
 
-public struct SearchParameters: Codable, Equatable {
-  /// Query string (mandatory).
-  public let query: String
+public struct SearchParameters: Encodable, Hashable {
+  public enum Filter: Encodable, Hashable {
+    case string(String)
+    case array([String])
+
+    public func encode(to encoder: Encoder) throws {
+      var container = encoder.singleValueContainer()
+
+      switch self {
+      case .string(let value):
+        try container.encode(value)
+
+      case .array(let value):
+        try container.encode(value)
+      }
+    }
+  }
+
+  public enum MatchingStrategy: String, Encodable, Hashable {
+    case all
+    case last
+  } 
+
+  /// Query string.
+  public let query: String?
 
   /// Number of documents to take.
-  public let limit: Int
+  public let limit: Int?
 
   /// Number of documents to skip.
-  public let offset: Int
+  public let offset: Int?
+
+  /// Request a specific page of results.
+  public let page: Int?
+
+  /// Maximum number of documents returned for a page.
+  public let hitsPerPage: Int?
 
   /// Document attributes to show.
-  public let attributesToRetrieve: [String]
+  public let attributesToRetrieve: [String]?
 
   /// Which attributes to crop.
   public let attributesToCrop: [String]?
 
   /// Length used to crop field values.
-  public let cropLength: Int
+  public let cropLength: Int?
+
+  /// Sets a string to mark crop boundaries.
+  public let cropMarker: String?
 
   /// Attributes whose values will contain highlighted matching terms.
   public let attributesToHighlight: [String]?
 
+  /// The string to be inserted before a highlighted word.
+  public let highlightPreTag: String?
+
+  /// The string to be inserted after a highlighted word.
+  public let highlightPostTag: String?
+
+  /// Adds a _matchesPosition object to the search response that contains the location of each occurrence of queried terms across all fields.
+  /// SeeAlso: [Official Documentation](https://docs.meilisearch.com/reference/api/search.html#show-matches-position)
+  public let showMatchesPosition: Bool?
+
   /// Filter queries by an attribute value.
-  public let filter: String?
+  public let filter: Filter?
 
   /// Sort search results according to the attributes and sorting order (`asc` or `desc`) specified
-  public let sort: [String]?
+  public let sort: Filter?
 
-  /// Retrieve the count of matching terms for each facets.
-  public let facetsDistribution: [String]?
+  /// Retrieve the count of matching terms for each facet.
+  public let facets: [String]?
 
   /// Defines whether an object that contains information about the matches should be returned or not.
-  public let matches: Bool
+  public let matchingStrategy: MatchingStrategy?
 
   public init(
-    query: String = "",
-    offset: Int = 0,
-    limit: Int = 20,
-    attributesToRetrieve: [String] = ["*"],
+    query: String? = nil,
+    offset: Int? = nil,
+    limit: Int? = nil,
+    page: Int? = nil,
+    hitsPerPage: Int? = nil,
+    attributesToRetrieve: [String]? = nil,
     attributesToCrop: [String]? = nil,
-    cropLength: Int = 200,
+    cropLength: Int? = nil,
+    cropMarker: String? = nil,
     attributesToHighlight: [String]? = nil,
-    filter: String? = nil,
-    sort: [String]? = nil,
-    facetsDistribution: [String]? = nil,
-    matches: Bool = false
+    highlightPreTag: String? = nil,
+    highlightPostTag: String? = nil,
+    showMatchesPosition: Bool? = nil,
+    filter: Filter? = nil,
+    sort: Filter? = nil,
+    facets: [String]? = nil,
+    matchingStrategy: MatchingStrategy? = nil
   ) {
     self.query = query
     self.offset = offset
     self.limit = limit
+    self.page = page
+    self.hitsPerPage = hitsPerPage
     self.attributesToRetrieve = attributesToRetrieve
     self.attributesToCrop = attributesToCrop
     self.cropLength = cropLength
+    self.cropMarker = cropMarker
     self.attributesToHighlight = attributesToHighlight
     self.filter = filter
     self.sort = sort
-    self.facetsDistribution = facetsDistribution
-    self.matches = matches
+    self.facets = facets
+    self.matchingStrategy = matchingStrategy
+    self.highlightPreTag = highlightPreTag
+    self.highlightPostTag = highlightPostTag
+    self.showMatchesPosition = showMatchesPosition
   }
 
   public static func query(_ value: String) -> Self {
@@ -67,14 +120,20 @@ public struct SearchParameters: Codable, Equatable {
   enum CodingKeys: String, CodingKey {
     case query = "q"
     case offset,
-         limit,
-         attributesToRetrieve,
-         attributesToCrop,
-         cropLength,
-         attributesToHighlight,
-         filter,
-         sort,
-         facetsDistribution,
-         matches
+      attributesToCrop,
+      attributesToHighlight,
+      attributesToRetrieve,
+      cropLength,
+      cropMarker,
+      facets,
+      filter,
+      highlightPreTag,
+      highlightPostTag,
+      hitsPerPage,
+      limit,
+      matchingStrategy,
+      page,
+      showMatchesPosition,
+      sort
   }
 }
