243181dbbff016f2a9f7b66fbf92a690aee23ac1
Reda Lemeden <git@redalemeden.com>
2023-02-18 11:04:03 +0100

    Update search result model

--- Sources/MeilisearchNIO/Models/SearchResult.swift
+++ Sources/MeilisearchNIO/Models/SearchResult.swift
@@ -1,31 +1,40 @@
 import Foundation
 
+/// The result of a search request.
+///
+/// SeeAlso: [Official Documentation](https://docs.meilisearch.com/reference/api/search.html#response)
 public struct SearchResult<T>: Codable, Equatable
   where T: Codable, T: Equatable {
   /// Possible hints from the search query.
   public let hits: [T]
 
   /// Number of documents skipped.
-  public let offset: Int
+  public let offset: Int?
 
   /// Number of documents taken.
-  public let limit: Int
+  public let limit: Int?
 
-  /// Total number of matches,
-  public let nbHits: Int
+  /// Current search results page.
+  public let page: Int?
 
-  /// Whether `nbHits` is exhaustive.
-  public let exhaustiveNbHits: Bool?
+  /// Estimated total number of matches.
+  public let estimatedTotalHits: Int?
+
+  /// Exhaustive total number of matches.
+  public let totalHits: Int?
+
+  /// Exhaustive total number of search result pages.
+  public let totalPages: Int?
+
+  /// Number of results on each page.
+  public let hitsPerPage: Int?
 
   /// Distribution of the given facets.
   public let facetsDistribution: [String: [String: Int]]?
 
-  /// Whether facetDistribution is exhaustive.
-  public let exhaustiveFacetsCount: Bool?
-
   /// Time, in milliseconds, to process the query.
-  public let processingTimeMs: Int?
+  public let processingTimeMs: Int
 
   /// Query string from the search.
-  public let query: String?
+  public let query: String
 }
