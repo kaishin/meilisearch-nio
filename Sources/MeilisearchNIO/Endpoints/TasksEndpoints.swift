4a8c1f215e524b4c505b5a8e1fb1617cf39d366f
Reda Lemeden <git@redalemeden.com>
2023-02-18 11:03:22 +0100

    Add task endpoints

---
+++ Sources/MeilisearchNIO/Endpoints/TasksEndpoints.swift
@@ -0,0 +1,76 @@
+import Foundation
+import NIOHTTP1
+import NIO
+import AsyncHTTPClient
+
+extension MeilisearchClient {
+  /// List all tasks globally, regardless of index. 
+  /// - Parameters:
+  ///   - eventLoop: The event loop to run the query on.
+  /// - Returns: An paged collection of tasks.
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/tasks.html#get-tasks)
+  public func getAllTasks(
+    with parameters: GetTaskQueryParameters = .init(),
+    on eventLoop: EventLoop? = nil
+  ) async throws -> Page<OperationTask> {
+    try await send(
+      .tasks,
+      requestQueries(parameters.toQueryParameters()),
+      on: eventLoop
+    )
+  }
+
+  /// Get a single task.
+  /// - Parameters:
+  ///   - taskID: The ID of the task.
+  ///   - eventLoop: The event loop to run the query on.
+  /// - Returns: The task details.
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/tasks.html#get-one-task)
+  public func getTask(
+    id: Int,
+    on eventLoop: EventLoop? = nil
+  ) async throws -> OperationTask {
+    try await send(
+      .tasks / id,
+      on: eventLoop
+    )
+  }
+
+  /// Cancel any number of enqueued or processing tasks based on their uid, status, type, indexUid, or the date at which they were enqueued, processed, or completed. Task cancellation is an atomic transaction: either all tasks are successfully deleted, or none are.
+  ///
+  /// - Parameters:
+  ///   - taskID: The ID of the task.
+  ///   - eventLoop: The event loop to run the query on.
+  /// - Returns: The cancellation task reference.
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/tasks.html#cancel-tasks)
+  public func cancelTask(
+    taskID: Int,
+    parameters: CancelTaskQueryParameters = .init(),
+    on eventLoop: EventLoop? = nil
+  ) async throws -> OperationTask.Reference {
+    try await send(
+      URLPath.tasks / .cancel,
+      requestQueries(parameters.toQueryParameters()),
+      on: eventLoop
+    )
+  }
+
+  /// Delete a finished (succeeded, failed, or canceled) task based on uid, status, type, indexUid, canceledBy, or date. Task deletion is an atomic transaction: either all tasks are successfully deleted, or none are.
+  ///
+  /// - Parameters:
+  ///   - taskID: The ID of the task.
+  ///   - eventLoop: The event loop to run the query on.
+  /// - Returns: The cancellation task reference.
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/tasks.html#cancel-tasks)
+  public func deleteTask(
+    taskID: Int,
+    parameters: DeleteTaskQueryParameters = .init(),
+    on eventLoop: EventLoop? = nil
+  ) async throws -> OperationTask.Reference {
+    try await send(
+      URLPath.tasks / .cancel,
+      requestQueries(parameters.toQueryParameters()),
+      on: eventLoop
+    )
+  }
+}
