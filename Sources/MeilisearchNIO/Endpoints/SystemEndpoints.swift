bd64f17d8959a435b63e11dad0534ce595c94420
Reda Lemeden <git@redalemeden.com>
2023-02-18 10:59:43 +0100

    Rename stats endpoint file

--- Sources/MeilisearchNIO/Endpoints/SystemEndpoints.swift
+++ Sources/MeilisearchNIO/Endpoints/SystemEndpoints.swift
@@ -8,7 +8,7 @@ extension MeilisearchClient {
   /// - Parameters:
   ///   - eventLoop: The event loop to run the query on.
   /// - Returns: The health status.
-  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/health.html#get-health)
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/health.html#health)
   public func getHealth(
     on eventLoop: EventLoop? = nil
   ) async throws -> Health {
@@ -22,7 +22,7 @@ extension MeilisearchClient {
   /// - Parameters:
   ///   - eventLoop: The event loop to run the query on.
   /// - Returns: The version number.
-  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/version.html#get-version-of-meilisearch)
+  /// - SeeAlso: [Online documentation](https://docs.meilisearch.com/reference/api/version.html#version)
   public func getVersion(
     on eventLoop: EventLoop? = nil
   ) async throws -> Version {
