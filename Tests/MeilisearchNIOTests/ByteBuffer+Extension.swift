0b9d440f2e3dd0a62ec2847860b8490567c221b3
Reda Lemeden <git@redalemeden.com>
2023-02-18 11:05:34 +0100

    Remove throws from ByteBuffer extension

--- Tests/MeilisearchNIOTests/ByteBuffer+Extension.swift
+++ Tests/MeilisearchNIOTests/ByteBuffer+Extension.swift
@@ -4,15 +4,15 @@ import NIO
 extension ByteBuffer {
   static func from<T>(_ model: T) throws -> Self where T: Encodable {
     let data = try JSONEncoder().encode(model)
-    return try from(data)
+    return from(data)
   }
 
-  static func from(_ string: String) throws -> Self {
+  static func from(_ string: String) -> Self {
     let data = string.data(using: .utf8) ?? .init()
-    return try from(data)
+    return from(data)
   }
 
-  static func from(_ data: Data) throws -> Self {
+  static func from(_ data: Data) -> Self {
     var byteBuffer = ByteBufferAllocator().buffer(capacity: data.count)
     byteBuffer.writeBytes(data)
     return byteBuffer
