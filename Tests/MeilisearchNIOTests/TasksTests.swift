dc3fcc9781c65e8ce76341fadfe764584a9dd386
Reda Lemeden <git@redalemeden.com>
2023-02-18 10:57:54 +0100

    Fix task error test

--- Tests/MeilisearchNIOTests/TasksTests.swift
+++ Tests/MeilisearchNIOTests/TasksTests.swift
@@ -17,7 +17,7 @@ final class TasksTests: XCTestCase {
           "indexedDocuments": 0
         },
         "error": {
-          "message": "Document doesn't have a `uid` attribute: `{\"name\":\"Bloodborne\",\"japaneseName\":\"Bloodborne\",\"platforms\":[\"PC\",\"Series X\"],\"releasedAtText\":\"2023-10-31\",\"id\":\"1B175464-2722-441A-88DD-A12549BD5390\",\"artwork\":[{\"fileName\":\"5a722da7049be40d\",\"type\":\"cover\",\"width\":865,\"height\":1298}],\"releasedAt\":720403200}`.",
+          "message": "Document doesn't have a `uid` attribute: `{\\"name\\":\\"Bloodborne\\",\\"japaneseName\\":\\"Bloodborne\\",\\"platforms\\":[\\"PC\\",\\"Series X\\"],\\"releasedAtText\\":\\"2023-10-31\\",\\"id\\":\\"1B175464-2722-441A-88DD-A12549BD5390\\",\\"artwork\\":[{\\"fileName\\":\\"5a722da7049be40d\\",\\"type\\":\\"cover\\",\\"width\\":865,\\"height\\":1298}],\\"releasedAt\\":720403200}`.",
           "code": "missing_document_id",
           "type": "invalid_request",
           "link": "https://docs.meilisearch.com/errors#missing_document_id"
@@ -31,10 +31,14 @@ final class TasksTests: XCTestCase {
       )
     )
 
-    let indexes = try await client.listIndexes()
-    XCTAssertEqual(indexes.limit, 20)
-    XCTAssertEqual(indexes.total, 1)
-    XCTAssertEqual(indexes.results[0].uid, "movies")
-    XCTAssertEqual(indexes.results[0].primaryKey, "id")
+    struct Game: Codable, Equatable, Identifiable {
+      let name: String
+      var id: String { name }
+    }
+
+    let task = try await client.getTask(id: 1)
+    XCTAssertEqual(task.uid, 1)
+    XCTAssertEqual(task.status.error?.code, .missingDocumentId)
+    XCTAssertEqual(task.status.error?.type, .invalidRequest)
   }
 }
