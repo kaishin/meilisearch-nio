b7cf8f86e4a98aef18e2d09f23e990bb9615c571
Reda Lemeden <git@redalemeden.com>
2023-02-18 11:03:30 +0100

    Update key model

--- Sources/MeilisearchNIO/Models/Key.swift
+++ Sources/MeilisearchNIO/Models/Key.swift
@@ -1,11 +1,53 @@
 import Foundation
 
 public struct Key: Codable, Equatable {
-  public var description: String?
-  public var key: String
-  public var actions: [Action]
-  public var indexes: [String]
-  public var expiresAt: Date?
-  public var createdAt: Date
-  public var updatedAt: Date
+  public let name: String?
+  public let description: String?
+  public let uid: String
+  public let key: String
+  public let actions: [Action]?
+  public let indexes: [String]?
+  public let expiresAt: Date?
+  public let createdAt: Date
+  public let updatedAt: Date
+}
+
+extension Key {
+  public struct CreatePayload: Codable {
+    public let name: String?
+    public let description: String?
+    public let uid: String?
+    public let actions: [Action]
+    public let indexes: [String]
+    public let expiresAt: Date
+
+    public init(
+      name: String? = nil,
+      description: String? = nil,
+      uid: String? = nil,
+      actions: [Action],
+      indexes: [String],
+      expiresAt: Date
+    ) {
+      self.name = name
+      self.description = description
+      self.uid = uid
+      self.actions = actions
+      self.indexes = indexes
+      self.expiresAt = expiresAt
+    }
+  }
+
+  public struct UpdatePayload: Codable {
+    public let name: String?
+    public let description: String?
+
+    public init(
+      name: String? = nil,
+      description: String? = nil
+    ) {
+      self.name = name
+      self.description = description
+    }
+  }
 }
